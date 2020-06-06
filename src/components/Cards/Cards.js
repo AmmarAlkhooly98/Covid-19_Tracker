81fa91e963f043e286367c92016a9d91abe9b040
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2020-06-06 10:51:29 +0300

    cards init

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Grid, Card, CardContent, Typography } from "@material-ui/core";
+
+import styles from "./Cards.module.css";
 
 const Cards = (props) => {
-	console.log('hii', props);
-	return (
-		<div>
-			<h1>Cards</h1>
-		</div>
-	);
+  console.log("hii", props);
+  return (
+    <div className={styles.container}>
+      <Grid container justify="center" spacing={3}>
+        <Grid item xs={12} md={6} lg={4} component={Card} justify="center">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              infected
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">infected</Typography>
+            <Typography variant="body2">Number of cases of Covid-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Grid } from "@material-ui/core";
 
 const CountryPicker = () => {
-	return (
-		<div>
-			<h1>Country Picker</h1>
-		</div>
-	);
+  return (
+    <div>
+      <Grid container xs={12}>
+        <h1>Country Picker</h1>
+      </Grid>
+    </div>
+  );
 };
 
 export default CountryPicker;
