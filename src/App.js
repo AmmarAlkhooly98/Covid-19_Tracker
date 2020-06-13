a2827f7583e791866d519bddd6c2722c4f7dbe43
AmmarAlkhooly98 <ammaralkhooly1@gmail.com>
2020-06-13 14:47:57 +0300

    footer now displays when data is fetched

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
 
   render() {
     const { data, country } = this.state;
+    console.log(data);
     return (
       <div className={styles.container}>
         <img className={styles.image} src={image} alt="COVID-19" />
@@ -29,15 +30,19 @@ class App extends React.Component {
         <Chart data={data} country={country} />
         <Table />
         <div className={styles.push}></div>
-        <div className={styles.footer}>
-          Made by:{" "}
-          <a href="https://www.linkedin.com/in/ammar-alkhooly/">
-            Ammar Alkhooly
-          </a>{" "}
-          | Email:{" "}
-          <a href="mailto:ammaralkhooly1@gmail.com">ammaralkhooly1@gmail.com</a>{" "}
-          | Phone: <a href="tel:+962780623701">+962780623701</a>
-        </div>
+        {data.confirmed ? (
+          <div className={styles.footer}>
+            Made by:{" "}
+            <a href="https://www.linkedin.com/in/ammar-alkhooly/">
+              Ammar Alkhooly
+            </a>{" "}
+            | Email:{" "}
+            <a href="mailto:ammaralkhooly1@gmail.com">
+              ammaralkhooly1@gmail.com
+            </a>{" "}
+            | Phone: <a href="tel:+962780623701">+962780623701</a>
+          </div>
+        ) : null}
       </div>
     );
   }
